39035fab48a8071414eae939eaac941343c5b6e4
applebee <applebee@applebee1558.com>
2025-11-11 21:21:00 -0800

    init

---
+++ ucsbece154a_controller.sv
@@ -0,0 +1,107 @@
+// ucsbece154a_controller.sv
+// All Rights Reserved
+// Copyright (c) 2025 UCSB ECE
+// Distribution Prohibited
+
+
+module ucsbece154a_controller (
+    input logic     [6:0] op_i, 
+    input logic     [2:0] funct3_i,
+    input logic           funct7b5_i,
+    input logic	          Zero_i,
+    output logic          RegWrite_o,
+    output logic    [2:0] ImmSrc_o,
+    output logic          ALUSrc_o,
+    output logic    [2:0] ALUControl_o,
+    output logic          MemWrite_o,
+    output logic    [1:0] ResultSrc_o,
+    output logic          PCSrc_o
+);
+
+
+ `include "ucsbece154a_defines.svh"
+
+
+ logic [11:0] controls;
+ logic Branch, Jump;
+ logic [1:0] ALUOp;
+
+
+//  TO DO: Implement main decoder 
+//  • Replace all `z` values with the correct values
+//  • Extend the code to implement jal and lui 
+
+
+ assign {RegWrite_o,	
+	ImmSrc_o, 
+        ALUSrc_o, // 0 is register 1 is immediate
+        MemWrite_o,
+        ResultSrc_o,
+	Branch, 
+	ALUOp,
+	Jump} = controls;
+
+
+ always_comb begin
+   case (op_i)
+
+	instr_lw_op:        controls = 12'b1_000_1_0_01_0_00_0;       // reg write, immsrc, alusrc, memwrite, resultsrc, branch, aluop, jump
+	instr_sw_op:        controls = 12'b0_001_1_1_xx_0_00_0;  
+	instr_Rtype_op:     controls = 12'b1_xxx_0_0_00_0_10_0;   
+	instr_beq_op:       controls = 12'b0_010_0_0_xx_1_01_0;   
+	instr_ItypeALU_op:  controls = 12'b1_000_1_0_00_0_10_0;
+    instr_instr_jal_op: controls = 12'b1_011_1_0_10_0_10_1;
+    instr_lui_op:       controls = 12'b1_100_1_0_11_0_10_0;
+	default:            controls = 12'bx_xxx_x_x_xx_0_xx_x;
+
+     //      `ifdef SIM
+     //      $warning("Unsupported op given: %h", op_i);
+     //       `else
+     //       ;
+     //       `endif
+   endcase
+ end
+
+//  TO DO: Implement ALU decoder by replacing all `z` values with the correct values
+
+logic RtypeSub;
+
+assign RtypeSub = funct7b5_i & op_i[5];
+
+always_comb begin
+    case(ALUOp)
+        ALUop_mem:                 ALUControl_o = ALUcontrol_add;
+        ALUop_beq:                 ALUControl_o = ALUcontrol_sub;
+        ALUop_other: 
+            case(funct3_i)
+                instr_addsub_funct3: 
+                        if(RtypeSub) ALUControl_o = ALUcontrol_sub;
+                        else         ALUControl_o = ALUcontrol_add;  
+                instr_slt_funct3:  ALUControl_o = ALUcontrol_slt;  
+                instr_or_funct3:   ALUControl_o = ALUcontrol_or;  
+                instr_and_funct3:  ALUControl_o = ALUcontrol_and;  
+                default:           ALUControl_o = 3'bxxx;
+                //     `ifdef SIM
+                //         $warning("Unsupported funct3 given: %h", funct3_i);
+                //     `else
+                //         ;
+                //     `endif  
+            endcase
+        default:
+        begin   
+            ALUControl_o = 3'bxxx;
+            `ifdef SIM
+                $warning("Unsupported ALUop given: %h", ALUOp);
+            `else
+                ;
+            `endif   
+        end
+    endcase
+end
+
+// TO DO: Generate properly PCSrc by replacing all `z` values with the correct values
+
+assign PCSrc_o = 1'bz; 
+
+endmodule
+
---
+++ ucsbece154a_datapath.sv
@@ -0,0 +1,101 @@
+// ucsbece154a_datapath.sv
+// All Rights Reserved
+// Copyright (c) 2025 UCSB ECE
+// Distribution Prohibited
+
+
+module ucsbece154a_datapath (
+    input  logic        clk, reset_i,
+    input  logic        PCSrc_i,
+    output logic [31:0] PC_o,
+    input  logic [31:0] Instr_i,
+    input  logic        RegWrite_i,
+    input  logic  [2:0] ImmSrc_i,
+    input  logic        ALUSrc_i,
+    input  logic  [2:0] ALUControl_i,
+    output logic        Zero_o,
+    output logic [31:0] ALUResult_o, WriteData_o,
+    input  logic [31:0] ReadData_i,
+    input  logic  [1:0] ResultSrc_i
+);
+
+logic [31:0] ImmExt;
+logic [31:0] ALUSrc_B;
+logic [31:0] RD1;
+logic [31:0] RD2;
+logic [31:0] Register_In;
+
+`include "ucsbece154a_defines.svh"
+
+/// Your code here
+
+// Use name "rf" for a register file module so testbench file work properly (or modify testbench file) 
+ucsbece154a_rf rf(
+    .clk        (clk),
+    .a1_i       (Instr_i[19:15]),
+    .a2_i       (Instr_i[24:20]),
+    .a3_i       (Instr_i[11:7]),
+    .rd1_o      (RD1),
+    .rd2_o      (RD2),
+    .wd3_i      (Register_In),
+);
+ucsbece154a_alu alu(
+    .a          (RD1),
+    .b          (ALUSrc_B),
+    .f          (ALUControl_i),
+    .result     (ALUResult_o)
+);
+/*
+module ucsbece154a_alu(
+  input  logic [31:0] a,
+  input  logic [31:0] b,
+  input  logic [2:0]  f,
+  output logic [31:0] result,
+  output logic        zero,
+  output logic        overflow,
+  output logic        carry,
+  output logic        negative
+);
+*/
+
+assign WriteData_o = RD2; // write data out is rd2
+
+always_comb begin // extend unit
+    case (ImmSrc_i) 
+        imm_Itype: ImmExt = {{20{instr[31]}}, instr[31:20]};
+        imm_Stype: ImmExt = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+        imm_Btype: ImmExt = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
+        imm_Jtype: ImmExt = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+        imm_Utype: ImmExt = {{12{instr[31]}}, instr[31:12]}; // not sure if right
+    endcase
+end
+
+always_comb begin // alu src mux
+    case (ALUSrc_i)
+        ALUSrc_reg: ALUSrc_B = RD2;
+        ALUSrc_imm: ALUSrc_B = ImmExt;
+    endcase
+end
+
+
+always_comb begin // result src mux
+    case (ResultSrc_i)
+        ResultSrc_lui: Register_In = ImmExt;
+        ResultSrc_ALU: Register_In = ALUResult_o;
+        ResultSrc_jal: Register_In = PCSrc_i + 4;
+        ResultSrc_load: Register_In = ReadData_i;
+    endcase
+end
+
+always_comb begin // pcnext mux
+    case (PCSrc_i)
+        1'b0: PC_o = PCSrc_i + 4;
+        1'b1: begin
+            PC_o = PCSrc_i + ImmExt;
+        end
+    endcase
+end
+
+
+assign PC_o = PCSrc_i + 4; // change this later...
+endmodule
