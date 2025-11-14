fafe4c17815afbe1c1f8b8d904562421e7c9d625
applebee <applebee@applebee1558.com>
2025-11-13 18:56:52 -0800

    add comments

--- ucsbece154a_datapath.sv
+++ ucsbece154a_datapath.sv
@@ -18,7 +18,7 @@ module ucsbece154a_datapath (
     input  logic [31:0] ReadData_i,
     input  logic  [1:0] ResultSrc_i
 );
-
+ // additional internal logics to handle 
 logic [31:0] ImmExt;
 logic [31:0] ALUSrc_B;
 logic [31:0] RD1;
@@ -26,13 +26,13 @@ logic [31:0] RD2;
 logic [31:0] Register_In;
 logic [31:0] PC_next;
 
-logic alu_overflow, alu_carry, alu_negative;//not used for now, add to pass lint
+logic alu_overflow, alu_carry, alu_negative;//not used for now, added to pass lint
 `include "ucsbece154a_defines.svh"
 
 /// Your code here
 
 // Use name "rf" for a register file module so testbench file work properly (or modify testbench file) 
-ucsbece154a_rf rf(
+ucsbece154a_rf rf( // register file
     .clk        (clk),
     .a1_i       (Instr_i[19:15]),
     .a2_i       (Instr_i[24:20]),
@@ -43,7 +43,7 @@ ucsbece154a_rf rf(
     .we3_i      (RegWrite_i)
 );
 
-ucsbece154a_alu alu(
+ucsbece154a_alu alu( // alu
     .a          (RD1),
     .b          (ALUSrc_B),
     .f          (ALUControl_i),
@@ -53,33 +53,21 @@ ucsbece154a_alu alu(
     .carry      (alu_carry),
     .negative   (alu_negative)
 );
-/*
-module ucsbece154a_alu(
-  input  logic [31:0] a,
-  input  logic [31:0] b,
-  input  logic [2:0]  f,
-  output logic [31:0] result,
-  output logic        zero,
-  output logic        overflow,
-  output logic        carry,
-  output logic        negative
-);
-*/
 
-assign WriteData_o = RD2; // write data out is rd2
+assign WriteData_o = RD2; // write data out is always rd2
 
-always_comb begin // extend unit
+always_comb begin // extend unit mux
     case (ImmSrc_i) 
         imm_Itype: ImmExt = {{20{Instr_i[31]}}, Instr_i[31:20]};
         imm_Stype: ImmExt = {{20{Instr_i[31]}}, Instr_i[31:25], Instr_i[11:7]};
         imm_Btype: ImmExt = {{19{Instr_i[31]}}, Instr_i[31], Instr_i[7], Instr_i[30:25], Instr_i[11:8], 1'b0};
         imm_Jtype: ImmExt = {{12{Instr_i[31]}}, Instr_i[19:12], Instr_i[20], Instr_i[30:21], 1'b0};
-        imm_Utype: ImmExt = {Instr_i[31:12], {12{1'b0}}}; // not sure if right
+        imm_Utype: ImmExt = {Instr_i[31:12], {12{1'b0}}};
         default: ImmExt = 32'b0; // prevent latch inferring (for lint/synthesis test) 
     endcase
 end
 
-always_comb begin // alu src mux
+always_comb begin // alu src mux to chose output on alusrc b
     case (ALUSrc_i)
         ALUSrc_reg: ALUSrc_B = RD2;
         ALUSrc_imm: ALUSrc_B = ImmExt;
@@ -87,7 +75,7 @@ always_comb begin // alu src mux
 end
 
 
-always_comb begin // result src mux
+always_comb begin // result src mux, to control where result to register file comes from
     case (ResultSrc_i)
         ResultSrc_lui: Register_In = ImmExt;
         ResultSrc_ALU: Register_In = ALUResult_o;
@@ -105,11 +93,11 @@ always_comb begin // pcnext mux
     endcase
 end
 
-always_ff @(negedge clk) begin
+always_ff @(negedge clk) begin // flip flop to increment the program counter
     PC_o <= PC_next;
 end
 
-always_ff @(negedge reset_i) begin
+always_ff @(negedge reset_i) begin // set program counter to 0 on reset
     if(!reset_i) begin
         PC_o <= 32'b0;
     end
